235bb33e8ea1ca4f777758368d6a0862545c73f1
Saïd <20957603+ayewo@users.noreply.github.com>
2023-09-09 12:50:28 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,20 @@ const config: CapacitorConfig = {
     CapacitorUpdater : {
       version: "1.0.1",
       autoUpdate: true,
+      partialUpdate: true,
+      partialUpdateBaseVersion: "1.0.0",
+
+      // these setttings are only valid for local development with the CLI
+      localS3: true,
+      localHost: "http://localhost.proxyman.io:5173",
+      localWebHost: "http://localhost.proxyman.io:5173",
+      localSupa: "http://localhost.proxyman.io:54321",
+      localSupaAnon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+
+      // these URLs are only valid for local development with the plugin
+      updateUrl: "https://localhost.proxyman.io:8881/updates",
+      statsUrl: "https://localhost.proxyman.io:8881/stats",
+      channelUrl: "https://localhost.proxyman.io:8881/channel_self"
     }
   }
 };
